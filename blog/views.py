a99e397003f015590bcd978ea7240ed8d2fe72a6
mixael442 <misaelcapo720@gmail.com>
2022-04-13 18:25:28 -0300

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
-from .forms import PostForm 
+from .forms import PostForm
 
 # Create your views here.
 
